c31a8ffa1ec34d9c8f6aae907b0ab9e4316bb004
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2023-06-22 15:33:23 +0300

    code cleanup

---
+++ src/models/chat/useChat.ts
@@ -0,0 +1,24 @@
+import { MessageType } from '@/components/message/Message.types';
+import axios from 'axios';
+import { useState } from 'react';
+
+const useChat = () => {
+ const [messages, setMessages] = useState<MessageType[]>([]);
+
+ const handleSendMessage = async (message: MessageType['content']) => {
+  const newMessages = [...messages, { role: 'user', content: message }];
+  setMessages(newMessages);
+  try {
+   const res = await axios.post<{ message: string }>('/api/send-message', newMessages);
+   console.log(res);
+   setMessages([...newMessages, { role: 'system', content: res.data.message }]);
+  } catch (error) {
+   setMessages(newMessages.slice(0, -1));
+   throw error;
+  }
+ };
+
+ return {messages, onMessageSend: handleSendMessage}
+};
+
+export { useChat };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,31 +4,39 @@ import { Inter } from 'next/font/google';
 import Message from '@/components/message/Message';
 import { FormEvent, useState } from 'react';
 import { MessageType } from '@/components/message/Message.types';
+import { useChat } from '@/models/chat/useChat';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
- const [messages, setMessages] = useState<MessageType[]>([]);
- const [inputValue, setInputValue] = useState<string>('');
+ const [inputValue, setInputValue] = useState<MessageType['content']>('');
+ const [error, setError] = useState<string>('');
+ const { messages, onMessageSend } = useChat();
 
  const handleSendMessage = async (e: FormEvent<HTMLFormElement>) => {
   e.preventDefault();
-  const newMessages = [...messages, { role: 'user', content: inputValue }];
-  setMessages(newMessages);
-
   try {
-   const res = await axios.post<{ message: string }>('/api/send-message', newMessages);
-   console.log(res);
-   setMessages([...newMessages, { role: 'system', content: res.data.message }]);
+   await onMessageSend(inputValue);
    setInputValue('');
-  } catch (error) {
-   setMessages(newMessages.slice(0, -1));
-   console.error(error);
+   setError('');
+  } catch (error: any) {
+   setError(error.message);
   }
  };
 
  return (
   <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+   {Boolean(error) && <div className='alert alert-error'>
+    <svg xmlns='http://www.w3.org/2000/svg' className='stroke-current shrink-0 h-6 w-6' fill='none' viewBox='0 0 24 24'>
+     <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+     />
+    </svg>
+    <span>{error}</span>
+   </div>}
    <div>
     {messages.map((message) => (
      <Message key={message.content} message={message} />
